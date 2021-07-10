fd58787d1b39002d13629cd4dd9727eb7b81b6de
Maxim Zelenev <maxim.zelenev@yandex.ru>
2021-07-10 22:48:31 +0300

    function setMapCanvasSizing

--- src/app.js
+++ src/app.js
@@ -808,6 +808,10 @@
                 /**
                  * TODO Task 2. Опишите функцию которая задаст размеры игрового поля
                  */
+                 $canvas.style.width  = `${width}px`;
+                 $canvas.style.height = `${height}px`;
+                 $canvas.width = `${width}`;
+                 $canvas.height = `${height}`;
                 return $canvas;
             }
             function drawMapField(canvas, map, width, height, cellSize) {
